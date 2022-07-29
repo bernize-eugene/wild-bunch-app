92f6d36dff53b123b49d81c17fdb877329782e6d
Bernize Eugene <bernize.eugene123@gmail.com>
2022-07-29 09:25:56 -0700

    Test classes without foreign keys

--- model.py
+++ model.py
@@ -18,10 +18,10 @@ class User(db.Model):
     phone = db.Column(db.String)
     username = db.Column(db.String)
     password = db.Column(db.String)
-    user_location_id = db.Column(
-        db.Integer, db.ForeignKey("user_location.user_location_id"))
-    user_event_id = db.Column(
-        db.Integer, db.ForeignKey("user_event.user_event_id"))
+    # user_location_id = db.Column(
+    #     db.Integer, db.ForeignKey("user_location.user_location_id"))
+    # user_event_id = db.Column(
+    #     db.Integer, db.ForeignKey("user_event.user_event_id"))
 
     def __repr__(self):
         return (
@@ -32,8 +32,8 @@ class User(db.Model):
             f"phone = {self.phone}\n"
             f"username = {self.username}\n"
             f"password = {self.password}\n"
-            f"{self.user_location_id}\n"
-            f"user_event_id = {self.user_event_id}"
+            # f"{self.user_location_id}\n"
+            # f"user_event_id = {self.user_event_id}"
         )
 
 
@@ -64,7 +64,7 @@ class User_Event(db.Model):
     user_event_id = db.Column(
         db.Integer, autoincrement=True, primary_key=True
     )
-    event_id = db.Column(db.Integer, db.ForeignKey("events.event_id"))
+    # event_id = db.Column(db.Integer, db.ForeignKey("events.event_id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     attending = db.Column(db.Boolean)
 
@@ -83,67 +83,73 @@ class Event(db.Model):
 
     event_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    event_location_id = db.Column(
-        db.Integer, db.ForeignKey("event_location.event_location_id"))
-    event_type = db.Column(
-        db.Integer, db.ForeignKey("event_types.event_type_id"))
+    # event_location_id = db.Column(
+    #     db.Integer, db.ForeignKey("event_location.event_location_id"))
+    # event_type = db.Column(
+    #     db.Integer, db.ForeignKey("event_types.event_type_id"))
     event_name = db.Column(db.String)
-    start_date = db.Column(db.DateTime)
-    end_date = db.Column(db.DateTime)
-    description = db.Column(db.Text)
-    url_graphic = db.Column(db.String)
+    # start_date = db.Column(db.DateTime)
+    # end_date = db.Column(db.DateTime)
+    # start_time = db.Column(db.DateTime)
+    # end_time = db.Column(db.DateTime)
+    # description = db.Column(db.Text)
+    # url_graphic = db.Column(db.String)
 
     def __repr__(self):
         return (
             f"event_id = {self.event_id}\n"
             f"user_id = {self.user_id}\n"
-            f"event_location_id = {self.event_location_id}\n"
-            f"event_type = {self.event_type_id}\n"
-            f"event_name = {self.name}\n"
-            f"start_date = {self.start_date}\n"
-            f"end_date = {self.end_date}\n"
-            f"description = {self.description}\n"
-            f"url_graphic = {self.url_graphic}"
+            # f"event_location_id = {self.event_location_id}\n"
+            # f"event_type = {self.event_type_id}\n"
+            f"event_name = {self.event_name}"
+            # f"start_date = {self.start_date}\n"
+            # f"end_date = {self.end_date}\n"
+            # f"start_time = {self.start_time}\n"
+            # f"end_time = {self.end_time}\n"
+            # f"description = {self.description}\n"
+            # f"url_graphic = {self.url_graphic}"
         )
 
 
-#Testing User_Location ######
-test_user_location = User_Location(
-    longitude="123456.123",
-    latitude="123456.123"
-)
-db.session.add(test_user_location)
+class Event_Location(db.Model):
+    """Event location longitude and latitude coordinates"""
 
-# Testing User ######
-test_user = User(
-    first_name="Test",
-    last_name="User",
-    email="test@test.com",
-    username="testuser",
-    password="password",
-    user_location_id=test_user_location
-)
-db.session.add(test_user)
+    __tablename__ = "event_location"
+
+    event_location_id = db.Column(
+        db.Integer, autoincrement=True, primary_key=True)
+    longitude = db.Column(db.Float)
+    latitude = db.Column(db.Float)
+
+    def __repr__(self):
+        return(
+            f"event_location_id = {self.event_location_id}\n"
+            f"longitude = {self.longitude}\n"
+            f"latitude = {self.latitude}"
+        )
 
 
-# Testing User_Event ######
-test_user_event = User_Event(
-    user_id=test_user, attending="True"
-)
-db.session.add(test_user_event)
+class Event_Type(db.Model):
+    """Returns type of event"""
 
-# Testing Event ######
-test_event = Event(
-    user_id=test_user,
-    event_location_id=test_event_location
-    event_type=)
+    __tablename__ = "event_types"
 
-db.session.commit()
+    event_type_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    type_name = db.Column(db.String)
+
+    def __repr__(self):
+        return (
+            f"event type id = {self.event_type_id}\n"
+            f"type name = {self.type_name}"
+        )
+
 
 """Database Connection"""
 
 
-def connect_to_db(flask_app, db_uri="postgresql:///wild_bunch_database", echo=True):
+def connect_to_db(flask_app,
+                  db_uri="postgresql:///wild_bunch_database",
+                  echo=False):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -152,6 +158,8 @@ def connect_to_db(flask_app, db_uri="postgresql:///wild_bunch_database", echo=Tr
     db.init_app(flask_app)
 
     print("Connected to the db!")
+
+    db.create_all()
 
 
 if __name__ == "__main__":
